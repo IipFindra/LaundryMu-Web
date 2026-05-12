2c167e30241a7d452120e2484885382711a9bc0e
Zubairromzi <zubairromzi@gmail.com>
2026-05-12 10:24:04 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,8 +57,9 @@
 
 <!-- Password -->
 <div class="relative">
+
+    <!-- Icon Lock -->
     <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
-        <!-- Icon Lock -->
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
             viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -66,8 +67,39 @@
         </svg>
     </span>
 
-    <input type="password" name="password" placeholder="Password"
-        class="w-full pl-10 pr-4 py-3 border rounded-lg focus:ring-2 focus:ring-[#3e51b5] outline-none">
+    <!-- Input Password -->
+    <input id="password"
+        type="password"
+        name="password"
+        placeholder="Password"
+        class="w-full pl-10 pr-12 py-3 border rounded-lg focus:ring-2 focus:ring-[#3e51b5] outline-none">
+
+    <!-- Tombol Mata -->
+    <button type="button"
+        onclick="togglePassword()"
+        class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-[#3e51b5]">
+
+    <svg id="eyeIcon"
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-5 w-5"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor">
+
+    <!-- Mata Dicoret -->
+    <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M13.875 18.825A10.05 10.05 0 0112 19
+        c-4.478 0-8.268-2.943-9.542-7
+        a9.956 9.956 0 012.223-3.592M6.228 6.228
+        A9.956 9.956 0 0112 5c4.478 0
+        8.268 2.943 9.542 7a9.97 9.97 0
+        01-4.293 5.226M15 12a3 3 0
+        11-6 0 3 3 0 016 0zm6 6L3 3"/>
+</svg>
+    </button>
 </div>
 
             <!-- Remember -->
@@ -90,6 +122,55 @@
 
     </div>
 </div>
+<script>
+function togglePassword() {
 
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.getElementById('eyeIcon');
+
+    if (passwordInput.type === 'password') {
+
+        passwordInput.type = 'text';
+
+        // Mata biasa
+        eyeIcon.innerHTML = `
+            <path stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 12a3 3 0 11-6 0
+                3 3 0 016 0z"/>
+
+            <path stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M2.458 12C3.732 7.943
+                7.523 5 12 5c4.478 0
+                8.268 2.943 9.542 7
+                -1.274 4.057-5.064 7
+                -9.542 7-4.477 0
+                -8.268-2.943-9.542-7z"/>
+        `;
+
+    } else {
+
+        passwordInput.type = 'password';
+
+        // Mata dicoret
+        eyeIcon.innerHTML = `
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13.875 18.825A10.05 10.05 0 0112 19
+                c-4.478 0-8.268-2.943-9.542-7
+                a9.956 9.956 0 012.223-3.592M6.228 6.228
+                A9.956 9.956 0 0112 5c4.478 0
+                8.268 2.943 9.542 7a9.97 9.97 0
+                01-4.293 5.226M15 12a3 3 0
+                11-6 0 3 3 0 016 0zm6 6L3 3"/>
+        `;
+    }
+}
+</script>
 </body>
 </html>
