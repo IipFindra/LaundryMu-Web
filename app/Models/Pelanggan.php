161fbaf92b638b4216c0176ef74089ec82922b86
lipFindra <rafirjndra@gmail.com>
2026-05-23 13:06:47 +0700

    update pelanggan

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -1,35 +1,22 @@
 <?php
-<<<<<<< HEAD
 
-namespace App\Models;
-
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Sanctum\HasApiTokens;
-
-class Pelanggan extends Authenticatable
-{
-    use HasApiTokens;
-=======
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable; 
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Pelanggan extends Authenticatable
 {
-    use HasFactory, Notifiable;
->>>>>>> 584ea24f99ca75bbf9d2a854d25f910dd4583c5c
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'pelanggan';
     protected $primaryKey = 'id_pelanggan';
 
-<<<<<<< HEAD
     // Disable updated_at since it doesn't exist in the schema
     const UPDATED_AT = null;
 
-=======
->>>>>>> 584ea24f99ca75bbf9d2a854d25f910dd4583c5c
     protected $fillable = [
         'nama_lengkap',
         'no_telepon',
@@ -37,8 +24,7 @@ class Pelanggan extends Authenticatable
         'otp_code',
         'otp_expires_at',
     ];
-<<<<<<< HEAD
-
+    
     protected $hidden = [
         'otp_code',
     ];
@@ -48,10 +34,3 @@ class Pelanggan extends Authenticatable
         return $this->hasMany(Pesanan::class, 'nama_pelanggan', 'nama_lengkap');
     }
 }
-=======
-    
-    protected $hidden = [
-        'otp_code',
-    ];
-}
->>>>>>> 584ea24f99ca75bbf9d2a854d25f910dd4583c5c
